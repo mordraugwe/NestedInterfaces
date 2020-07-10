26fa3458d18adf8dc00d5546b8b0e7f404ddee95
Nikita Larionov <mykyta.larionov@3shape.com>
2020-07-10 09:13:54 +0300

    Initial commit

---
+++ NestedInterfaces/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NestedInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    
+    public interface IInterface
+    {
+        public interface INestedInterface<T>
+        {
+        }
+        public interface INestedSubInterface<T1, T2> : INestedInterface<T1>
+            where T2 : INestedSubInterface<T1, T2>.IDoubleNestedInterface
+        {
+            public interface IDoubleNestedInterface
+            {
+            }
+        }
+    }
+}
